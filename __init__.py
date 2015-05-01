8d363b3c6109285d3dc7a02f864af2f4731bc681
Teszko <b.iwaniuk@campus.tu-berlin.de>
2015-05-01 23:57:02 +0200

    imlemented encryption, not working yet

--- __init__.py
+++ __init__.py
@@ -15,15 +15,13 @@ class Enigma():
         'V': 'I',  'W': 'K',  'R': 'N',  'Q': 'O',  'U': 'P',  'T': 'S'
     }]
 
-    def __init__(self):
-        """
-        self.__rotor_L = Enigma.BARREL[rotorLid]
-        self.__rotor_M = Enigma.BARREL[rotorMid]
-        self.__rotor_R = Enigma.BARREL[rotorRid]
-        self.__rotor_L_position = 1
-        self.__rotor_M_position = 1
-        self.__rotor_R_position = 1
-        """
+    def __init__(self, barrel_ids, barrel_positions):
+        self.__barrel_L = Enigma.BARREL[barrel_ids[0]]
+        self.__barrel_M = Enigma.BARREL[barrel_ids[1]]
+        self.__barrel_R = Enigma.BARREL[barrel_ids[2]]
+        self.__barrel_L_position = barrel_positions[0]
+        self.__barrel_M_position = barrel_positions[1]
+        self.__barrel_R_position = barrel_positions[2]
         self.pinboard = {}
 
     @staticmethod
@@ -40,13 +38,50 @@ class Enigma():
 
     @staticmethod
     def barrel_encode(barrel, input_char):
-        return Enigma.BARREL[barrel][Enigma.ctoi(input_char)-1]
+        return barrel[Enigma.ctoi(input_char)-1]
 
     @staticmethod
     def barrel_encode_rev(barrel, input_char):
-        return Enigma.itoc(Enigma.BARREL[barrel].index(input_char)+1)
+        return Enigma.itoc(barrel.index(input_char)+1)
 
+    @staticmethod
+    def ctoi_barrel(barrel, a):
+        return Enigma.BARREL[barrel].index(a)+1
 
+    @staticmethod
+    def itoc_barrel(barrel, i):
+        return Enigma.BARREL[barrel][i-1]
+
+    @staticmethod
+    def shift_char_barrel(barrel, char, shift):
+        return Enigma.itoc_barrel(barrel, (Enigma.ctoi_barrel(barrel, char) + shift) % len(Enigma.BARREL[barrel]))
+
+    def increment_barrels(self):
+        self.__barrel_R_position += 1
+        if self.__barrel_R_position == len(Enigma.ALPHABET):
+            self.__barrel_M_position += 1
+            if self.__barrel_M_position == len(Enigma.ALPHABET):
+                self.__barrel_L_position += 1
+                self.__barrel_L_position %= len(Enigma.ALPHABET)
+            self.__barrel_M_position %= len(Enigma.ALPHABET)
+        self.__barrel_R_position %= len(Enigma.ALPHABET)
+
+    def print_barrel_positions(self):
+        print "(", self.__barrel_L_position, ",", self.__barrel_M_position, ",", self.__barrel_R_position, ")"
+
+    def encode(self, input_char):
+        self.increment_barrels()
+        msg = input_char
+        msg = self.pinboard_encode(msg)
+        msg = Enigma.barrel_encode(self.__barrel_R, self.shift_char_barrel(2, msg, -self.__barrel_R_position))
+        msg = Enigma.barrel_encode(self.__barrel_M, self.shift_char_barrel(1, msg, self.__barrel_M_position))
+        msg = Enigma.barrel_encode(self.__barrel_L, self.shift_char_barrel(0, msg, self.__barrel_L_position))
+        msg = Enigma.UKW[0][msg]
+        msg = Enigma.barrel_encode_rev(self.__barrel_L, self.shift_char_barrel(0, msg, 0))
+        msg = Enigma.barrel_encode_rev(self.__barrel_M, self.shift_char_barrel(1, msg, -self.__barrel_M_position))
+        msg = Enigma.barrel_encode_rev(self.__barrel_R, self.shift_char_barrel(2, msg, self.__barrel_R_position))
+        msg = self.pinboard_encode(Enigma.shift_char(msg, self.__barrel_R_position))
+        return msg
 
     def set_pinboard(self, connections):
         self.pinboard = {x[0]: x[1] for x in connections}
@@ -58,17 +93,12 @@ class Enigma():
         else:
             return char
 
-
-
-
-
-
-print Enigma.UKW[0]['L']
-print Enigma.ctoi('B')
-print Enigma.itoc(12)
-eni = Enigma()
+eni = Enigma((0, 1, 2), (24, 25, 25))
 eni.set_pinboard(['AE', 'BJ', 'CM', 'DZ', 'FL', 'GY', 'HX', 'IV', 'KW', 'NR'])
-print eni.pinboard_encode('C')
-print eni.pinboard
-print Enigma.shift_char('A', 1)
-print Enigma.barrel_encode_rev(0, 'O')
+bla1 = [eni.encode('H'), eni.encode('E'), eni.encode('L'), eni.encode('L'), eni.encode('O')]
+print bla1
+
+eni2 = Enigma((0, 1, 2), (24, 25, 25))
+eni2.set_pinboard(['AE', 'BJ', 'CM', 'DZ', 'FL', 'GY', 'HX', 'IV', 'KW', 'NR'])
+bla2 = [eni.encode(bla1[0]), eni.encode(bla1[1]), eni.encode(bla1[2]), eni.encode(bla1[3]), eni.encode(bla1[4])]
+print bla2
